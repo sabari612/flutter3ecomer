e28cd8c9c5f486c6f46b6703adb15844a66b82ac
sabari612 <114132660+sabari612@users.noreply.github.com>
2024-04-01 13:46:08 +0530

    Add files via upload

---
+++ dartpad/web_plugin_registrant.dart
@@ -0,0 +1,28 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:package_info_plus_web/package_info_plus_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  PackageInfoPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
